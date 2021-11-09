dfe129bca88d5d341b7d3d7727fd2e79fa28c730
pananfly <906792655@qq.com>
2021-11-09 16:02:58 +0800

    Push init test codes.

---
+++ Test/test.cpp
@@ -0,0 +1,64 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+// 设置窗口尺寸
+const unsigned int SCR_WIDTH = 400;
+const unsigned int SCR_HEIGHT = 300;
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    // glfw: 初始化
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    // uncomment this statement to fix compilation on OS X
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+#endif
+    // glfw 创建窗口
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Test<GLFW+GLAD>", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.0f, 1.f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        // glfwPollEvents();
+        glfwWaitEvents();
+    }
+    // glfw: terminate, clearing all previously allocated GLFWresources.
+    glfwTerminate();
+    return 0;
+}
